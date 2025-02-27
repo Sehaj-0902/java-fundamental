b83680a238a2c0db0043e87ca6d3311aaf0b7782
Sehaj-0902 <sehajpreet1296.be21@chitkara.edu.in>
2025-02-27 14:33:30 +0530

    [Sehaj] ADD: Program for Celsius to Fahrenheit conversion

--- CelsiusToFahrenheit.java
+++ CelsiusToFahrenheit.java
@@ -6,6 +6,6 @@ public class CelsiusToFahrenheit {
         System.out.println("Enter temperature in Celsius: ");
         double celsius = sc.nextDouble();
         double fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
-        System.out.println(celsius + " degrees Celsius is equal to " + fahrenheit + " degrees Fahrenheit.");
+        System.out.println(celsius + " degrees Celsius is equal to " + fahrenheit + " degrees Fahrenheit");
     }
 }
