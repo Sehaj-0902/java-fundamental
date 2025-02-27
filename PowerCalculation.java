71f6d87805921da66558b45056e3cd5b2a7c9939
Sehaj-0902 <sehajpreet1296.be21@chitkara.edu.in>
2025-02-27 15:42:33 +0530

    [Sehaj] ADD: Program to calculate base to the power of exponent

--- PowerCalculation.java
+++ PowerCalculation.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class PowerCalculation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the base: ");
+        System.out.println("Enter the Base: ");
         double base = sc.nextDouble();
-        System.out.println("Enter the exponent: ");
+        System.out.println("Enter the Exponent: ");
         double exponent = sc.nextDouble();
         double result = Math.pow(base, exponent);
         System.out.println(base + " raised to the power of " + exponent + " is: " + result);
