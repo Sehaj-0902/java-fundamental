d871549ccde851d6facb21bb06d2ffa2ebc8f811
Sehaj-0902 <sehajpreet1296.be21@chitkara.edu.in>
2025-02-27 14:36:36 +0530

    [Sehaj] ADD: Program for Kilometers to Miles conversion

--- KilometersToMiles.java
+++ KilometersToMiles.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class KilometersToMiles {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter distance in Kilometers: ");
+        System.out.println("Enter distance in kilometers: ");
         double kilometers = sc.nextDouble();
         double miles = kilometers * 0.621371;
         System.out.println(kilometers + " kilometers is equal to " + miles + " miles");
